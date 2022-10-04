b0ac9aa23bb8ccb42148a4259bdb5c72a24b5684
leephh <114863171+leephh@users.noreply.github.com>
2022-10-04 22:35:37 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,13 +3,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
         alert("点击了图片");
-        targetImage.classList.add("small");
-        targetImage.classList.remove("small");
+       
         if (targetImage.classList.contains("small")) {
-            
+            targetImage.classList.remove("small");
         } else {
-            // ...
+            targetImage.classList.add("small");
             
+            
+            
+
         }
     });
 });
