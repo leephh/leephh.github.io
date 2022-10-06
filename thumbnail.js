8d2e75b7223c9ec750df29c480076ead9b1f401e
leephh <114863171+leephh@users.noreply.github.com>
2022-10-06 15:47:09 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
-       
+        
         if (targetImage.classList.contains("small")) {
             targetImage.classList.remove("small");
         } else {
@@ -11,4 +11,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
     });
+    document.getElementById("btn").onclick=function(){send()};//设置按钮触发事件（函数send（））
+    function send(){
+        var value= document.getElementById("typing").value;//获得文本框里的内容
+        var p= document.createElement("p");//创建新元素，也即结点
+        p.innerHTML='<hr>'+value;//往结点里塞内容
+        document.getElementById("commend").prepend(p); 
+    }
+
 });
+
